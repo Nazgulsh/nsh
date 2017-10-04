85fc008b42162ed1bd7299c6e69dca9c63e1cbdd
Nazgul <nazgulkyrgyzbay@gmail.com>
2017-10-04 21:47:20 +0600

    Екінші коммит

--- src/qwerty/Hello.java
+++ src/qwerty/Hello.java
@@ -3,5 +3,7 @@ package qwerty;
 public class Hello {
     public static void main(String[] args) {
         System.out.print("Hello World");
+        System.out.print(" Бұл зертханалық жұмыстың мақсаты");
+        System.out.print("GitHub-пен жұмыс жасап үйрену");
     }
 }
