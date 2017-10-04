0ec44229d19d2ca86117027ec713fabc138fd3da
Nazgul <nazgulkyrgyzbay@gmail.com>
2017-10-04 22:01:03 +0600

    Hello.java файлы аздап озгерды

--- src/qwerty/Hello.java
+++ src/qwerty/Hello.java
@@ -5,5 +5,10 @@ public class Hello {
         System.out.print("Hello World");
         System.out.print(" Бұл зертханалық жұмыстың мақсаты");
         System.out.print("GitHub-пен жұмыс жасап үйрену");
+
+        int a=MyMath.kvadrat( 5);
+        int b=MyMath.kub( 5);
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
     }
 }
