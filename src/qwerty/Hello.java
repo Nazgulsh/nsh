f5105523d0c6ca380efc424448d83748b7bb97f0
Nazgul <nazgulkyrgyzbay@gmail.com>
2017-10-04 22:30:07 +0600

    experiment1 бұтағында Hello.java  файлына тағы  озгерыс жасалды

--- src/qwerty/Hello.java
+++ src/qwerty/Hello.java
@@ -10,6 +10,7 @@ public class Hello {
         int b=MyMath.kub( 5);
         System.out.println("a= "+a);
         System.out.println("b= "+b);
-        System.out.println(MyMath.max( 8,  9));
+        System.out.println("max="+ MyMath.max3( 8,  9, 8));
+        System.out.println("min="+ MyMath.min3(12,8,15));
     }
 }
